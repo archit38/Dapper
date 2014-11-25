0c6b3db79709d3865a4bc24435be3e85041c4096
archit38 <architbhardwaj88@gmail.com>
2014-11-25 14:47:05 +0000

    new models and bootstrap

---
+++ DapperExample/Models/League.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DapperExample.Dapper
+{
+    public class League
+    {
+        public int LeagueID { get; set; }
+
+        public string Name { get; set; }
+
+        public int RunPoints { get; set; }
+
+        public int WicketPoints { get; set; }
+
+        public int CatchPoints { get; set; }
+
+        public int RunoutPoints { get; set; }
+
+        public int WicketKeeperOffset { get; set; }
+
+        public int CaptainPoints { get; set; }
+
+        public int WinPointsPoints { get; set; }
+
+        public int ExtraRuleID { get; set; }
+
+        //public int List<Challenge> Challenges { get; set; }
+        //public int List<Match> Matches { get; set; }
+    }
+}
